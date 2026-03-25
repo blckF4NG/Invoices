0924954b32045c7c0aabd66501f94291d43ab6a9
Daksh Sati <daksh.sati.4@gmail.com>
2026-03-25 13:18:31 +0530

    back to safe version: not automation

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -18,20 +18,23 @@ async function main() {
 
   console.log('Admin user created/updated.', adminJ);
 
-  const companies = [
-    {
+  const ssdAviation = await prisma.company.create({
+    data: {
       name: 'SSD Aviation Techno Pvt. Ltd.',
       address: 'Plot No 49, Kami Road, Asadpur Nandnaur, Kami-Ganur Road Sonipat, Haryana - 131027',
       stateCode: '06',
       gstin: '06ABOCS1545E1ZC',
       pan: '',
       signatoryName: 'Authorised Signatory',
-      logoImagePath: '/assets/ssd_logo.jpg',
+      logoImagePath: '/assets/ssd_logo.jpg', // Using assets from client spec, wait, spec says DB Seed copies to /uploads
       bankName: 'HDFC Bank',
       bankAccount: '50200104459853',
       ifscCode: 'HDFC0007876'
-    },
-    {
+    }
+  });
+
+  const fabLoc1 = await prisma.company.create({
+    data: {
       name: 'FAB Aviation (Location 1)',
       address: 'Enter address for FAB Aviation Location 1',
       stateCode: 'XX',
@@ -42,8 +45,11 @@ async function main() {
       bankName: '',
       bankAccount: '',
       ifscCode: ''
-    },
-    {
+    }
+  });
+
+  const fabLoc2 = await prisma.company.create({
+    data: {
       name: 'FAB Aviation (Location 2)',
       address: 'Enter address for FAB Aviation Location 2',
       stateCode: 'XX',
@@ -55,15 +61,7 @@ async function main() {
       bankAccount: '',
       ifscCode: ''
     }
-  ];
-
-  for (const company of companies) {
-    await prisma.company.upsert({
-      where: { id: companies.indexOf(company) + 1 }, // Simple way for default companies
-      update: company,
-      create: company,
-    });
-  }
+  });
 
   const uploadsDir = path.join(__dirname, '..', 'uploads');
   if (!fs.existsSync(uploadsDir)) {
